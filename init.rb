b4bb75ecca71802e2bd9593725a7464821e0004f
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2014-10-17 21:35:32 +0400

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_cache do
+  name 'Redmine cache plugin'
+  author 'Dmitry Yu Okunev'
+  description "Enables caching for a few pages"
+  version '0.0.1'
+end
